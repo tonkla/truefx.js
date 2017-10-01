45c5220ef67f4e95fbb9982c3bb9f87e832515cf
Surakarn Samkaew <tonkla@gmail.com>
2017-10-01 21:34:03 +0700

    Complete get() with unit tests

--- src/truefx.js
+++ src/truefx.js
@@ -1,16 +1,60 @@
-module.exports = {
+const csv = require('fast-csv')
+const request = require('request')
+
+const truefx = {
   authenticate: function (username, password) {
     this.session = `${username}:${password}:sid`
     return this
   },
 
   get: function (symbol = '') {
-    const url = this.buildUrl(symbol)
-    return url
+    return new Promise((resolve, reject) => {
+      const url = this.buildUrl(symbol)
+      request(url, (error, response, body) => {
+        if (error) reject(error)
+        this.parseCSV(body)
+            .then(output => { resolve(output) })
+      })
+    })
+  },
+
+  parseCSV: function (csvStr) {
+    return new Promise((resolve, reject) => {
+      const output = []
+      let tick = {}
+      csv.fromString(csvStr)
+        .on('data', (data) => {
+          if (data.length > 0) {
+            tick = {
+              symbol: data[0],
+              timestamp: data[1],
+              bid: parseFloat(data[2] + data[3]),
+              offer: parseFloat(data[4] + data[5]),
+              low: parseFloat(data[6]),
+              high: parseFloat(data[7]),
+              open: parseFloat(data[8])
+            }
+            tick.spread = parseFloat(this.getSpread(tick))
+            output.push(tick)
+          }
+        })
+        .on('end', () => {
+          resolve(output)
+        })
+    })
+  },
+
+  getSpread: function (tick) {
+    if (/jpy/.test(tick.symbol.toLowerCase())) {
+      return Math.round(((tick.offer - tick.bid) * 100) * 100) / 100
+    } else {
+      return Math.round(((tick.offer - tick.bid) * 10000) * 100) / 100
+    }
   },
 
   buildUrl: function (symbol = '') {
-    let baseUrl = 'https://webrates.truefx.com/rates/connect.html?f=csv'
+    // NOTE: HTTPS raises `sslv3 alert handshake failure`
+    let baseUrl = 'http://webrates.truefx.com/rates/connect.html?f=csv'
     if (this.session !== undefined) {
       baseUrl += `&id=${this.session}`
     }
@@ -34,3 +78,5 @@ module.exports = {
     return symbol
   }
 }
+
+module.exports = truefx
--- test/truefx.js
+++ test/truefx.js
@@ -2,9 +2,22 @@
 /* eslint no-undef: "error" */
 
 const assert = require('assert')
+const { expect } = require('chai')
 const truefx = require('../lib/index')
 
 describe('TrueFX', function () {
+  describe('authenticate()', function () {
+    it('should have authenticate()', function () {
+      expect(truefx).to.respondsTo('authenticate')
+    })
+  })
+
+  describe('get()', function () {
+    it('should have get()', function () {
+      expect(truefx).to.respondsTo('get')
+    })
+  })
+
   describe('sanitizeUrl()', function () {
     it('should work for single symbol with slash', function () {
       assert.equal(truefx.sanitizeSymbol('eur/usd'), 'eur/usd')
@@ -28,7 +41,7 @@ describe('TrueFX', function () {
   })
 
   describe('buildUrl()', function () {
-    const baseUrl = 'https://webrates.truefx.com/rates/connect.html?f=csv'
+    const baseUrl = 'http://webrates.truefx.com/rates/connect.html?f=csv'
 
     it('should work without symbol', function () {
       assert.equal(truefx.buildUrl(), baseUrl)
@@ -42,6 +55,44 @@ describe('TrueFX', function () {
       const session = 'u:p:s'
       truefx.session = session
       assert.equal(truefx.buildUrl('eurusd'), `${baseUrl}&id=${session}&c=EUR/USD`)
+    })
+  })
+
+  describe('getSpread()', function () {
+    it('should work with 5 digits pair', function () {
+      const tick = {
+        symbol: 'EUR/USD',
+        bid: 1.00001,
+        offer: 1.00006
+      }
+      assert.equal(truefx.getSpread(tick), 0.5)
+    })
+
+    it('should work with 3 digits pair', function () {
+      const tick = {
+        symbol: 'USD/JPY',
+        bid: 1.002,
+        offer: 1.006
+      }
+      assert.equal(truefx.getSpread(tick), 0.4)
+    })
+  })
+
+  describe('parseCSV()', function () {
+    it('should parse CSV input to correct JSON ouput', function () {
+      const input = 'EUR/USD,1506717900444,1.18,159,1.18,167,1.17724,1.18328,1.17863'
+      return truefx.parseCSV(input).then(res => {
+        expect(res).to.eql([
+          { symbol: 'EUR/USD',
+            timestamp: '1506717900444',
+            bid: 1.18159,
+            offer: 1.18167,
+            low: 1.17724,
+            high: 1.18328,
+            open: 1.17863,
+            spread: 0.8 }
+        ])
+      })
     })
   })
 })
